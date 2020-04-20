318bdceb65a3bbebbe0cfd811265cab89e955010
Saul Amster <saulamster@gmail.com>
2020-04-20 15:54:13 -0400

    Added navmesh grid work

---
+++ HCI_Final_Project/Assets/Scripts/AIGridGenerator.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.AI;
+
+using System.Threading.Tasks;
+using UnityEditor;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+public class AIGridGenerator : MonoBehaviour
+{
+    [BoxGroup("Settings")] public NavMeshSurface navMesh;
+    [BoxGroup("Settings")] public Transform player;
+
+    [BoxGroup("Settings")] public float pointDistance;
+    [BoxGroup("DEBUG")] public bool showPoints;
+    [BoxGroup("DEBUG")][ReadOnly] public int numberOfPoints = 0;
+
+    private string fileName = "";
+
+    private List<AIGridPoint> points;
+    private List<SearializableAIGridPoint> serializedPoints;
+    private Bounds gridBounds;
+    
+
+    // public void OnBeforeSerialize() {
+
+    // }
+
+    // public void OnAfterDeserialize() {
+    //     if(points == null) {
+    //         deserializeFromList();
+    //     }
+    // }
+
+    private string getFileName() {
+        string newFileName = "Assets/AI/" + NavMesh.GetSettingsNameFromID(navMesh.agentTypeID).Replace(" ", "") + "_AIGridPoints.dat";
+        if(!newFileName.Equals(fileName)) {
+            fileName = newFileName;
+        }
+        return fileName;
+    }
+
+    //TODO: Make sure this works for builds
+    public void serializeToList() {
+        if(serializedPoints == null) serializedPoints = new List<SearializableAIGridPoint>();
+        serializedPoints.Clear();
+        foreach(AIGridPoint p in points) {
+            serializedPoints.Add(new SearializableAIGridPoint() {
+                pointX = p.point.x,
+                pointY = p.point.y,
+                pointZ = p.point.z
+            });
+        }
+        
+        FileStream fs = new FileStream(getFileName(), FileMode.Create);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(fs, serializedPoints);
+        fs.Close();
+    }
+
+    [Button("Load Grid")]
+    public void deserializeFromList() {
+        string path = getFileName();
+        print("Reading in points from: " + path);
+
+        if(!File.Exists(path)) {
+            return;
+        }
+        
+        FileStream fs = new FileStream(fileName, FileMode.Open);
+        BinaryFormatter bf = new BinaryFormatter();
+        serializedPoints = (List<SearializableAIGridPoint>)bf.Deserialize(fs);
+        fs.Close();
+
+        if(points == null) points = new List<AIGridPoint>();
+        points.Clear();
+        foreach(SearializableAIGridPoint p in serializedPoints) {
+            points.Add(new AIGridPoint(new Vector3(p.pointX, p.pointY, p.pointZ)));
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        if(points == null || points.Count == 0) {
+            deserializeFromList();
+        }
+
+        if(showPoints && points != null) {
+            AIGridPoint point;
+            float colorVal;
+            if(Application.isPlaying) {
+                for(int i = 0; i < points.Count; i++) {
+                    if(Random.Range(0, 9) % 10 != 0 || points[(int)i].sqrProximityToPlayer > 1000f) continue;
+                    colorVal = points[i].sqrProximityToPlayer / 1000f;
+                    colorVal = Mathf.Round(colorVal * 3) / 3f;
+                    Gizmos.color = new Color(colorVal, 1 - colorVal, 0);
+                    point = points[i];
+                    Gizmos.DrawLine(point.getPoint() + Vector3.forward * pointDistance / 3f, point.getPoint() - Vector3.forward * pointDistance / 3f);
+                    Gizmos.DrawLine(point.getPoint() + Vector3.right * pointDistance / 3f, point.getPoint() - Vector3.right * pointDistance / 3f);
+                }
+            } else {
+                for(int i = 0; i < points.Count; i++) {
+                    int index = i;
+                    colorVal = points[index].sqrProximityToPlayer / 1000f;
+                    Gizmos.color = new Color(colorVal, 1 - colorVal, 0);
+                    point = points[index];
+                    Gizmos.DrawLine(point.getPoint() + Vector3.forward * pointDistance / 3f, point.getPoint() - Vector3.forward * pointDistance / 3f);
+                    Gizmos.DrawLine(point.getPoint() + Vector3.right * pointDistance / 3f, point.getPoint() - Vector3.right * pointDistance / 3f);
+                }
+                
+            }
+            // section = (section + 1) % NUM_SECTIONS;
+        }
+    } 
+
+    [Button("Generate Grid")]
+    public void generateGrid() {
+        points = new List<AIGridPoint>();
+        numberOfPoints = 0;
+        gridBounds = new Bounds(navMesh.navMeshData.sourceBounds.center, navMesh.navMeshData.sourceBounds.size);
+        print("Generating NavMesh with bounds: " + gridBounds.center + " | " + gridBounds.size);
+        int numPointsX = Mathf.RoundToInt(gridBounds.size.x / pointDistance);
+        int numPointsY = Mathf.RoundToInt(gridBounds.size.y / pointDistance);
+        int numPointsZ = Mathf.RoundToInt(gridBounds.size.z / pointDistance);
+        float halfDistX = gridBounds.size.x / 2f;
+        float halfDistY = gridBounds.size.y / 2f;
+        float halfDistZ = gridBounds.size.z / 2f;
+        NavMeshQueryFilter filter = new NavMeshQueryFilter();
+        filter.agentTypeID = navMesh.agentTypeID;
+        filter.areaMask = ~0;
+        NavMeshHit hit;
+        Vector3 point;
+
+        for(int x = 0; x < numPointsX; x++) {
+            for(int y = 0; y < numPointsY; y++) {
+                for(int z = 0; z < numPointsZ; z++) {
+                    point = new Vector3((x * pointDistance) - halfDistX, (y * pointDistance) - halfDistY, (z * pointDistance) - halfDistZ);
+                    //print(point);
+                    if(NavMesh.SamplePosition(point, out hit, pointDistance, filter)) {
+                        
+                        if(hit.position.y < point.y)
+                            points.Add(new AIGridPoint(point));
+                        
+                        // print("Found point: " + point);
+                    }
+                }
+            }
+        }
+
+        numberOfPoints = points.Count;
+        //Undo.RecordObject(points, "Generated AI Points");
+        serializeToList();
+
+        print("Found " + points.Count + " points");
+    }
+
+
+    void Start() {
+        //generateGrid();
+        deserializeFromList();
+    }
+
+    void Update() {
+        if(points != null) {
+            Vector3 playerPosition = player.position;
+            // foreach(AIGridPoint p in points) {
+            //     p.sqrProximityToPlayer = Vector3.SqrMagnitude(p.getPoint() - playerPosition);
+            // }
+            Parallel.ForEach(points, (p, state) => {
+                p.sqrProximityToPlayer = Vector3.SqrMagnitude(p.getPoint() - playerPosition);
+            });
+        }
+        
+    }
+}
+
+
---
+++ HCI_Final_Project/Assets/Scripts/AIGridPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AIGridPoint {
+    [SerializeField]
+    
+    public Vector3 point;
+    public float sqrProximityToPlayer;
+
+    public AIGridPoint(Vector3 point) {
+        this.point = point;
+    }
+
+    public Vector3 getPoint() {
+        return point;
+    }
+}
+
+[System.Serializable]
+public struct SearializableAIGridPoint {
+    public float pointX;
+    public float pointY;
+    public float pointZ;
+}
