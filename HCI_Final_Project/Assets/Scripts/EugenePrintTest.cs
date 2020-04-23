3c01b800171f5103460b5d6bc25cae39556df086
Eugene Kuznetsov <Azurekuz@gmail.com>
2020-04-23 18:26:00 -0400

    Created a barren test map. Created initial spawn encounters. Ran into issue.

---
+++ HCI_Final_Project/Assets/Scripts/EugenePrintTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EugenePrintTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestPrint(){
+        Debug.Log("Trigger01 Entered");
+    }
+}
