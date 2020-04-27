cb56e73133832b7264ecbf8a693ae379ba98e253
Eugene Kuznetsov <Azurekuz@gmail.com>
2020-04-27 18:01:46 -0400

    Made a test scene with ProBuilder. Have triggers and encounters. Used Nav Links. Animations aren't working properly for some reason.

--- HCI_Final_Project/Assets/Scripts/TriggerOnce.cs
+++ HCI_Final_Project/Assets/Scripts/TriggerOnce.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EugenePrintTest : MonoBehaviour
+public class TriggerOnce : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class EugenePrintTest : MonoBehaviour
         
     }
 
-    public void TestPrint(){
-        Debug.Log("Trigger01 Entered");
+    public void runOnce(){
+        gameObject.SetActive(false);
     }
 }
