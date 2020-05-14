00c7e87c6f5f826eb9451ba657adc3452e0bb989
Saul Amster <saulamster@gmail.com>
2020-05-14 00:03:16 -0400

    added core prefab/functionality and victory condition

---
+++ HCI_Final_Project/Assets/CoreController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreController : MonoBehaviour
+{
+    public HealthController health;
+    public Transform player;
+    public Transform upperLid;
+    public Transform lowerLid;
+    public Transform eye;
+    public AnimationCurve lidEasing;
+    public float lidAnimDuration = 1f;
+    public Vector2 upperlidAngles;
+    public Vector2 lowerlidAngles;
+    private float lidTimestamp = -1000f;
+    private bool coreEnabled = false;
+    void Start()
+    {
+        // startCore();
+        if(player == null) {
+            //TODO: not this
+            player = GameObject.Find("Player").transform;
+        }
+        health.invincible = true;
+    }
+
+    void Update()
+    {
+        eye.LookAt(player.position + Vector3.up);
+
+        float progress = Mathf.Clamp((Time.time - lidTimestamp) / lidAnimDuration, 0, 1f);
+        progress = coreEnabled ? 1f - progress : progress;
+        float upperValue = Mathf.Lerp(upperlidAngles.x, upperlidAngles.y, progress);
+        float lowerValue = Mathf.Lerp(lowerlidAngles.x, lowerlidAngles.y, progress);
+        upperLid.localEulerAngles = new Vector3(0, 90, upperValue);
+        lowerLid.localEulerAngles = new Vector3(0, 90, lowerValue);
+
+    }
+
+    public void startCore() {
+        lidTimestamp = Time.time;
+        coreEnabled = true;
+        health.invincible = false;
+    }
+}
---
+++ HCI_Final_Project/Assets/PlayerVictoryController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerVictoryController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject victoryPanel;
+    public GameObject tilter;
+    public GameObject crosshair;
+    public GameObject deathCam;
+    public TimerSystem timer;
+    public Text victoryTimeText;
+
+    void Start()
+    {
+        victoryPanel.SetActive(false);
+        deathCam.SetActive(false);
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.R) && deathCam.activeInHierarchy) {
+            resetLevel();
+        }
+    }
+
+    public void Victory() {
+        victoryPanel.SetActive(true);
+        player.SetActive(false);
+        deathCam.SetActive(true);
+        tilter.SetActive(false);
+        crosshair.SetActive(false);
+        victoryTimeText.text = timer.splits[timer.splits.Count - 1].time.ToString("n2");
+    }
+
+    public void resetLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+}
