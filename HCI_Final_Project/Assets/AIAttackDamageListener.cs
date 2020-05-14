7f2181edfa5eeb4c0abd8f8d15e4bc900ad23836
Saul Amster <saulamster@gmail.com>
2020-05-14 13:05:44 -0400

    spiders now do damage (poorly)

---
+++ HCI_Final_Project/Assets/AIAttackDamageListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttackDamageListener : MonoBehaviour
+{
+    public float damageAmount = 5;
+    private bool isAttacking;
+    private HashSet<HealthController> attackHits;
+
+    void OnCollisionEnter(Collision collision) {
+        HealthController hc = collision.collider.GetComponent<HealthControllerReferencer>()?.healthController;
+        if(isAttacking && !attackHits.Contains(hc)) {
+            attackHits.Add(hc);
+            hc.recieveDamage(transform.position, damageAmount);
+        }
+    }
+
+    public void startAttack() {
+        isAttacking = true;
+        attackHits = new HashSet<HealthController>();
+    }
+
+    public void endAttack() {
+        isAttacking = false;
+    }
+}
